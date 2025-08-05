50d90314e6fa30d4946e0d75d89a8a7f4fce5e4e
최겸준/인사서비스개발팀/ND <jun1718@nhndooray.com>
2025-08-05 20:57:09 +0900

    first commit

---
+++ src/main/java/com/human/infinite/power/reportly/ReportlyApplication.java
@@ -0,0 +1,13 @@
+package com.human.infinite.power.reportly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/human/infinite/power/reportly/ReportlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.human.infinite.power.reportly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
