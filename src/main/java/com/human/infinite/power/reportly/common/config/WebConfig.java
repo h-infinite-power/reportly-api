46efb1590105b3098e66401d5ea76e7b46ad43c9
getsoss <getsoss@naver.com>
2025-08-07 18:04:26 +0900

    [Fix] CORS 설정

--- src/main/java/com/human/infinite/power/reportly/common/config/WebConfig.java
+++ src/main/java/com/human/infinite/power/reportly/common/config/WebConfig.java
@@ -7,13 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/*")
-			.allowedOrigins("") // 모든 오리진 허용
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-			.allowedHeaders("*")
-			.allowCredentials(false) // 모든 오리진 허용 시 false로 설정해야 함
-			.maxAge(3600);
-	}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
+    }
 }
